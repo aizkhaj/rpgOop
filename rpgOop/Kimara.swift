f5616ca895a59c3e268a6907048dcc45b89e0073
Aizaz K <az_2099@hotmail.com>
2015-11-29 03:35:12 -0600

    kimara.swift added/completed

---
+++ rpgOop/Kimara.swift
@@ -0,0 +1,31 @@
+//
+//  Kimara.swift
+//  rpgOop
+//
+//  Created by Aizazuddin Khaja on 2015-11-29.
+//  Copyright © 2015 A2Z. All rights reserved.
+//
+
+import Foundation
+
+class Kimara: Enemy {
+    
+    let IMMUNE_MAX = 15
+    
+    override var loot: [String] {
+        return ["Tough Hide", "Kimara Venom", "Rare Trident"]
+    }
+    
+    override var type: String {
+        return "Kimara"
+    }
+    
+    override func attemptAttack(attackPwr: Int) -> Bool {
+        if attackPwr >= IMMUNE_MAX {
+            return super.attemptAttack(attackPwr) //you can use super.# to call parent item..at least in this case
+        } else {
+            return false
+        }
+    }
+    
+}
