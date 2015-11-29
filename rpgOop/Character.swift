05304f7931e365bd8642adcd05150fe323c2eae8
Aizaz K <az_2099@hotmail.com>
2015-11-29 03:06:41 -0600

    base class in progress for character.swift

---
+++ rpgOop/Character.swift
@@ -0,0 +1,35 @@
+//
+//  Character.swift
+//  rpgOop
+//
+//  Created by Aizazuddin Khaja on 2015-11-29.
+//  Copyright © 2015 A2Z. All rights reserved.
+//
+
+import Foundation
+
+class Character {
+    
+    private var _hp: Int = 100
+    private var _attackPwr: Int = 10
+    
+    var attackPwr: Int {
+        get {
+            return _attackPwr
+        }
+    }
+    
+    var hp: Int {
+        get {
+            return _hp
+        }
+    }
+    
+    init(startingHp: Int, attackPwr: Int) {
+        self._hp = startingHp
+        self._attackPwr = attackPwr
+    }
+    
+    
+}
+
