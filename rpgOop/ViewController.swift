b2390afc78433ad1c105a1e6ec81463b1e6e2fae
Aizaz K <az_2099@hotmail.com>
2015-11-29 14:05:05 -0600

    created player and enemy variables in viewcontroller, set basic logic for player's characteristics and enemy characteristics. added function to generate random enemy and used if statement to define which enemy can appear based on what integer value is produced at random.

--- rpgOop/ViewController.swift
+++ rpgOop/ViewController.swift
@@ -21,12 +21,30 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var chestBtn: UIButton!
     
+    var player: Player!
+    var enemy: Enemy!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        player = Player(name: "DirtyLaundry21", hp: 110, attackPwr: 20)
+        
+        playerHpLbl.text = "\(player.hp) HP"
     }
 
+    func generateRandomEnemy() {
+        let rand = Int(arc4random_uniform(2))
+        
+        if rand == 0 {
+            enemy = Kimara(startingHp: 50, attackPwr: 12)
+        } else {
+            enemy = DevilWizard(startingHp: 60, attackPwr: 15)
+        }
+    }
+    
     @IBAction func onChestTapped(sender: AnyObject) {
     }
 
