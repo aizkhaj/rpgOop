69a7c9846c34c47354af9d3c67c5a84a90f4df6b
Aizaz K <az_2099@hotmail.com>
2015-11-28 19:07:03 -0600

    Initial Commit

---
+++ rpgOop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rpgOop
+//
+//  Created by Aizazuddin Khaja on 2015-11-28.
+//  Copyright © 2015 A2Z. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
