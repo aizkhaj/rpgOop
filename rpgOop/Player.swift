07dfef1eda08640fdf58da5902b62400245e54d7
Aizaz K <az_2099@hotmail.com>
2015-11-29 03:21:51 -0600

    player save 1

--- rpgOop/Player.swift
+++ rpgOop/Player.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+class Player: Character {
+    
+    private var _name = "Player"
+    
+    var name: String {
+        get {
+            return _name
+        }
+    }
+    
+    private var _inventory = [String]()
+    
+    var inventory: [String] {
+        return _inventory
+    }
+    
+    convenience init(name: String, hp: Int, attackPwr: Int) {
+        self.init(startingHp: hp, attackPwr: attackPwr)
+        
+        _name = name
+    }
+}
